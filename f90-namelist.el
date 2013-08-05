22e2c10f132c2d5afc01a56bce672ceace574300
Peter Hill <peter.hill@cea.fr>
2013-08-05 10:13:13 +0200

    Initial commit
    Add basic functions for inserting a new namelist: inside-nml, to check if point is
    inside a namelist; and insert-f90-namelist-safe, to insert a new namelist without
    breaking the current ones.

---
+++ f90-namelist.el
@@ -0,0 +1,23 @@
+;; f90-namelist
+;; An extension to f90-mode for Fortran namelists
+;;
+;; D. Dickinson and P. Hill
+;; 2013
+
+(defun inside-nml ()
+  "Returns t if currently inside a namelist and nil if not"
+  (interactive)
+  (setq st (save-excursion (re-search-backward "&" 0 t)))
+  (setq en (save-excursion (re-search-backward "/" 0 t)))
+  ( > st en))
+
+(defun insert-f90-namelist-safe ()
+  "Insert a new f90 namelist"
+  (interactive "")
+  (if (inside-nml)
+      (message "Inside a namelist!")
+    (setq nml-name (read-from-minibuffer "Namelist name:"))
+    (insert (format "\n&%s" nml-name))
+    (funcall f90-auto-keyword-case -1)
+    (insert "\n\n/")
+    (previous-line)))
